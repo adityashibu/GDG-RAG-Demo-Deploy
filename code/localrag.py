e305d397ba0e140057f035637d6b5b93c09f54e2
Aditya Shibu <as2397@hw.ac.uk>
2025-01-05 19:33:34 +0400

    Add initial implementation of localrag.py with imports and color definitions

---
+++ code/localrag.py
@@ -0,0 +1,14 @@
+import torch
+import ollama
+import os
+from openai import OpenAI
+import argparse
+import json
+
+# ANSI escape codes for colors
+PINK = '\033[95m'
+CYAN = '\033[96m'
+YELLOW = '\033[93m'
+NEON_GREEN = '\033[92m'
+RESET_COLOR = '\033[0m'
+
