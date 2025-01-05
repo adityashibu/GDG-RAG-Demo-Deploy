de0ceefdfc7c2827f9f118f5494d694ed8fa0309
Aditya Shibu <as2397@hw.ac.uk>
2025-01-05 19:45:02 +0400

    Add file opening function to localrag.py for reading file contents

--- code/localrag.py
+++ code/localrag.py
@@ -12,3 +12,9 @@ YELLOW = '\033[93m'
 NEON_GREEN = '\033[92m'
 RESET_COLOR = '\033[0m'
 
+# Function to open a file an return its contents as a string
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+    
+# Function to get the relevant context from the vault based on the user input
